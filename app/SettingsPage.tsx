8f68eecdeee51ee6255932e3ec4e4c552683f8e9
Eugene <eugenekang03@gmail.com>
2025-05-15 15:16:34 +0100

    begin of work

--- app/SettingsPage.tsx
+++ app/SettingsPage.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function HomePage() {
+export default function SettingsPage() {
   return (
     <View
       style={{
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from "expo-router";
+import { useRouter } from "expo-router";
+import MapPage from "./MapPage";
 
 export default function RootLayout() {
-  return <Stack />;
+  const router = useRouter();
+  return (
+    <div>
+      <MapPage />
+      {/* <Button onPress={() => router.navigate("/MapPage")}>Maps</Button> */}
+    </div>
+  );
 }
