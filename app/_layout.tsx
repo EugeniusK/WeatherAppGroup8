657c4d490b711842ba7edcfaab4bc4ee2d9c2431
Eugene <eugenekang03@gmail.com>
2025-05-20 14:28:56 +0100

    update .json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from "expo-router";
-// import SettingsPage from "./SettingsPage";
-import HomeScreen from "./index";
+import SettingsPage from "./SettingsPage";
 // import MapPage from "./MapPage";
 import React from "react";
 
@@ -8,7 +7,7 @@ export default function RootLayout() {
   const router = useRouter();
   return (
     // <div>
-    <HomeScreen />
+    <SettingsPage />
     // {/* <Button onPress={() => router.navigate("/MapPage")}>Maps</Button> */}
     // </div>
   );
