adbd850fd87038237f69a74af7a383f5de7f1874
Zerui Chen <34794550+Zerui18@users.noreply.github.com>
2025-05-22 15:28:48 +0100

    global state 2/2

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,13 @@
-import { useRouter } from "expo-router";
-import SettingsPage from "./SettingsPage";
-// import MapPage from "./MapPage";
 import React from "react";
+import { useRouter } from "expo-router";
+import { AppProvider } from "../utils/context";
+import SettingsPage from "./SettingsPage";
 
 export default function RootLayout() {
-  const router = useRouter();
+
   return (
-    // <div>
-    <SettingsPage />
-    // {/* <Button onPress={() => router.navigate("/MapPage")}>Maps</Button> */}
-    // </div>
+    <AppProvider>
+      <SettingsPage />
+    </AppProvider>
   );
 }
